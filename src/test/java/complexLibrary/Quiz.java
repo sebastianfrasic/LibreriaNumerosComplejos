87290d38e4c51ffd8280e38e1abe2b900fc3d0a3
unknown <2145120@labinfo.is.escuelaing.edu.co>
2019-09-17 15:31:36 -0500

    Intento quiz 5

--- src/test/java/complexLibrary/Quiz.java
+++ src/test/java/complexLibrary/Quiz.java
@@ -7,7 +7,57 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class Quiz {
-
+    
+    
+    @Test
+    public void xd() {
+        MatrizCompleja A = new MatrizCompleja(3, 3);
+    	MatrizCompleja V = new MatrizCompleja(3, 1);
+        
+        try{
+            A.addComplex(0, 0, new NumeroComplejo(0, 0));
+            A.addComplex(0, 1, new NumeroComplejo((double)1/6, 0));
+            A.addComplex(0, 2, new NumeroComplejo((double)5/6, 0));
+            A.addComplex(1, 0, new NumeroComplejo((double)1/3, 0));
+            A.addComplex(1, 1, new NumeroComplejo((double)1/2, 0));
+            A.addComplex(1, 2, new NumeroComplejo((double)1/6, 0));
+            A.addComplex(2, 0, new NumeroComplejo((double)2/3, 0));
+            A.addComplex(2, 1, new NumeroComplejo((double)1/3, 0));
+            A.addComplex(2, 2, new NumeroComplejo(0, 0));
+            
+            V.addComplex(0, 0, new NumeroComplejo((double)1/5, 0));
+            V.addComplex(1, 0, new NumeroComplejo((double)7/10, 0));
+            V.addComplex(2, 0, new NumeroComplejo((double)1/10, 0));
+            
+            System.out.println(Simulador.sistemaConDinamicaDeterminista(A, V, 4));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        
+    }
+    
+    @Test
+    public void xdd() {
+        MatrizCompleja A = new MatrizCompleja(2, 2);
+    	MatrizCompleja V = new MatrizCompleja(2, 1);
+        
+        try{
+            A.addComplex(0, 0, new NumeroComplejo((double)1/Math.sqrt(2), 0));
+            A.addComplex(0, 1, new NumeroComplejo(0, (double)1/Math.sqrt(2)));
+            
+            A.addComplex(1, 0, new NumeroComplejo((double)1/Math.sqrt(2), 0));
+            A.addComplex(1, 1, new NumeroComplejo(0, (double)-1/Math.sqrt(2)));
+            
+            V.addComplex(0, 0, new NumeroComplejo((double)1, 0));
+            V.addComplex(1, 0, new NumeroComplejo((double)0, 0));
+            
+            
+            System.out.println(Simulador.sistemaConDinamicaDeterminista(A, V, 1));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        
+    }
     /**
      * ¿Es la matriz compleja una matriz unitaria?  ---- True
      */
